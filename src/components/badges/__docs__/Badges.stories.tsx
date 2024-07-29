db1b8bfb7be2bf4f4e08b741a8c06e04ca06c7e9
Kiran <kiran.kumal714@gmail.com>
2024-07-29 22:15:10 +0545

    chore: refactor stories for badges

--- src/components/badges/__docs__/Badges.stories.tsx
+++ src/components/badges/__docs__/Badges.stories.tsx
@@ -1,11 +1,10 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { Badges } from "../Badges";
+import { Badges, badgeVariants } from "../Badges";
 
 const meta: Meta = {
   title: "components/Badges",
   component: Badges,
   tags: ["autodocs"],
-  args: { variant: "green" },
   argTypes: {
     variant: {
       options: ["default", "yellow", "red", "green", "blue", "primary"],
@@ -17,7 +16,7 @@ const meta: Meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Green: Story = {
+export const Default: Story = {
   args: {
     children: "Badges",
   },
