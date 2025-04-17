c19aae9b63b85dca65a5eb5569441da550649eb2
Shiva Aryal <aryalshiva005@gmail.com>
2025-04-17 16:44:36 +0545

    Implement code changes to enhance functionality and improve performance across various modules.

---
+++ src/components/accordion/components/Accordion.tsx
@@ -0,0 +1,35 @@
+import React, { useId } from "react";
+import { AccordionItem } from "./AccordionItem";
+import { AccordionProps } from "../types";
+import useAccordionState from "../hook/useAccordionState";
+import { ChevronUpIcon, ChevronDownIcon } from "../assets/icons";
+
+const Accordion: React.FC<AccordionProps> = ({
+  items,
+  allowMultiple = false,
+  UpIcon = ChevronUpIcon,
+  DownIcon = ChevronDownIcon,
+}) => {
+  const id = useId();
+  const { openItems, handleToggle } = useAccordionState(allowMultiple);
+
+  return (
+    <div className="border border-gray-200 rounded-md" role="tablist">
+      {items.map((item, index) => (
+        <AccordionItem
+          key={index + id}
+          title={item.title}
+          open={openItems.includes(index)}
+          onToggle={handleToggle}
+          index={index}
+          UpIcon={UpIcon}
+          DownIcon={DownIcon}
+        >
+          {item.children}
+        </AccordionItem>
+      ))}
+    </div>
+  );
+};
+
+export default Accordion;
