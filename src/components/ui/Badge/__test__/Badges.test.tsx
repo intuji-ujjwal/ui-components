815725d2014884fb7de3fb456926a936f76755ba
ujjwal-intuji <ujjwal.bhandari@intuji.com>
2025-04-22 09:35:24 +0545

    chore: resolved import type

--- src/components/ui/Badge/__test__/Badges.test.tsx
+++ src/components/ui/Badge/__test__/Badges.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
-import { Badges } from '../Badges';
+import { Badges } from '../Badge';
 
 describe('Badges Component', () => {
   it('should render with default variant', () => {
