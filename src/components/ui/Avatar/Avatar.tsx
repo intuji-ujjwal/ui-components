d23e7c3830380406f4783cb54b32c0571bd72a2b
ujjwal-intuji <ujjwal.bhandari@intuji.com>
2025-04-24 16:29:48 +0545

    feat: avatar component

--- src/components/ui/Avatar/Avatar.tsx
+++ src/components/ui/Avatar/Avatar.tsx
@@ -1,74 +1,41 @@
-import React from 'react';
+import { memo, useMemo } from 'react';
+
+import { AvatarProps } from './types';
 import { cn } from '../../../utils/cn';
-import useMediaQuery from '../../../hooks/useMediaQuery';
+import ImageComponent from '../Image/Image';
+import { avatarVariants } from './helper/styles';
 
-export interface AvatarProps {
-  classes?: string;
-  avatar: {
-    id: React.Key | null | undefined;
-    imageSrc: string | undefined;
-    name: string | undefined;
-  }[];
-  size?: number;
-  borderWidth?: number;
-}
-
-const Avatar: React.FC<AvatarProps> = ({
-  classes,
-  avatar,
-  size,
-  borderWidth,
-}: AvatarProps) => {
-  const isBigScreen = useMediaQuery('(min-width: 1200px)');
-  const isDesktopOrLaptop = useMediaQuery('(min-width: 991px)');
-  const isTablet = useMediaQuery('(min-width: 768px)');
-  const isMobile = useMediaQuery('(min-width: 300px)');
-
-  if (!avatar || avatar.length < 1) return null;
-
-  let maxAvatarsToShow = 2;
-  if (isBigScreen) {
-    maxAvatarsToShow = 5;
-  } else if (isDesktopOrLaptop) {
-    maxAvatarsToShow = 4;
-  } else if (isTablet) {
-    maxAvatarsToShow = 3;
-  } else if (isMobile) {
-    maxAvatarsToShow = 2;
-  }
-
-  const avatarsToShow = avatar.slice(0, maxAvatarsToShow);
-  const remainingCount = avatar.length - maxAvatarsToShow;
-
-  const defaultclass = `rounded-full border border-gray-500 overflow-hidden block first:ml-0 -ml-2 before:content-[''] before:block before:pt-[100%] before:bg-white relative`;
+export const Avatar = memo(function Avatar({
+  className,
+  imageSrc,
+  name,
+  size = 'md',
+  type,
+  ...props
+}: AvatarProps) {
+  const initial = useMemo(
+    () => (imageSrc ? undefined : name.charAt(0).toUpperCase()),
+    [imageSrc, name],
+  );
 
   return (
-    <div className='avatar'>
-      {avatar && (
-        <div className='avatar-wrap flex items-center'>
-          {avatarsToShow.map((item) => (
-            <figure
-              className={cn(defaultclass, classes)}
-              key={item?.id}
-              style={{
-                width: size ? size + 'px' : '70px',
-                borderWidth: borderWidth ? borderWidth + 'px' : '4px',
-              }}
-            >
-              <img
-                src={item?.imageSrc}
-                alt={item?.name}
-                className='absolute left-0 top-0 h-full w-full object-cover'
-              />
-            </figure>
-          ))}
-          {remainingCount > 0 && (
-            <span className='ml-2 text-sm'>+{remainingCount} more</span>
-          )}
+    <figure
+      className={cn(avatarVariants({ size, type }), className)}
+      data-cy={`avatar size:${size} - name:${name}`}
+      {...props}
+    >
+      {imageSrc ? (
+        <ImageComponent
+          alt={name}
+          src={imageSrc}
+          data-cy={name}
+          className='absolute left-0 top-0 h-full w-full object-cover'
+        />
+      ) : (
+        <div className='bg-grey-100 absolute left-0 right-0 top-0 flex h-full items-center justify-center rounded-full object-cover'>
+          <p className='text-xs font-semibold text-primary-500'>{initial}</p>
         </div>
       )}
-    </div>
+    </figure>
   );
-};
-
-export default Avatar;
+});
