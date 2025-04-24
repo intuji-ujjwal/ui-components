648838317fef4d7e5b6bd30e63a4bb51c89472ab
ujjwal-intuji <ujjwal.bhandari@intuji.com>
2025-04-24 16:30:42 +0545

    chore: avatar docs

--- src/components/ui/Avatar/__docs__/Avatar.stories.tsx
+++ src/components/ui/Avatar/__docs__/Avatar.stories.tsx
@@ -1,73 +1,32 @@
+import { Avatar as AvatarComponent } from '../Avatar';
 import type { Meta, StoryObj } from '@storybook/react';
-import Avatar from '../Avatar';
-
-// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
-
-const avatars = [
-  {
-    id: 0,
-    imageSrc:
-      'https://images.pexels.com/photos/25184994/pexels-photo-25184994/free-photo-of-fashion-eastern-dresses.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    name: 'avatar 1',
-  },
-  {
-    id: 1,
-    imageSrc:
-      'https://images.pexels.com/photos/25184994/pexels-photo-25184994/free-photo-of-fashion-eastern-dresses.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    name: 'avatar 1',
-  },
-  {
-    id: 2,
-    imageSrc:
-      'https://images.pexels.com/photos/25184994/pexels-photo-25184994/free-photo-of-fashion-eastern-dresses.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    name: 'avatar 1',
-  },
-  {
-    id: 3,
-    imageSrc:
-      'https://images.pexels.com/photos/25184994/pexels-photo-25184994/free-photo-of-fashion-eastern-dresses.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    name: 'avatar 1',
-  },
-  {
-    id: 4,
-    imageSrc:
-      'https://images.pexels.com/photos/25184994/pexels-photo-25184994/free-photo-of-fashion-eastern-dresses.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    name: 'avatar 1',
-  },
-  {
-    id: 5,
-    imageSrc:
-      'https://images.pexels.com/photos/25184994/pexels-photo-25184994/free-photo-of-fashion-eastern-dresses.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    name: 'avatar 1',
-  },
-  {
-    id: 6,
-    imageSrc:
-      'https://images.pexels.com/photos/25184994/pexels-photo-25184994/free-photo-of-fashion-eastern-dresses.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    name: 'avatar 1',
-  },
-  {
-    id: 7,
-    imageSrc:
-      'https://images.pexels.com/photos/25184994/pexels-photo-25184994/free-photo-of-fashion-eastern-dresses.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    name: 'avatar 1',
-  },
-];
 
 const meta: Meta = {
   title: 'components/Avatar',
-  component: Avatar,
+  component: AvatarComponent,
   tags: ['autodocs'],
-} satisfies Meta<typeof Avatar>;
+} satisfies Meta<typeof AvatarComponent>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
+export const Avatar: Story = {
   args: {
-    classes: '',
-    avatar: avatars,
-    size: 70,
-    borderWidth: 4,
+    name: 'avatar 1',
+    imageSrc: 'https://photogov.net/_nuxt/img/result.266b50d.webp',
+    size: '3xl',
+  },
+  argTypes: {
+    size: {
+      options: ['xs', 'sm', 'md', 'lg', 'xl', '2xl', '3xl'],
+      control: { type: 'select' },
+    },
+  },
+};
+
+export const AvatarWithNoImage: Story = {
+  args: {
+    name: 'Sam',
+    size: '3xl',
   },
 };
